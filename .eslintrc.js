956e6339cf237655c500fbbd101459b90355f420
Zephraph <zephraph@gmail.com>
2019-04-02 00:39:59 -0400

    Add auto, grab config...

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,11 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+  parser: "@typescript-eslint/parser",
+  extends: ["plugin:@typescript-eslint/recommended", "prettier/@typescript-eslint", "plugin:prettier/recommended"],
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  rules: {},
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": false,
+  },
 };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import { Application } from "probot"; // eslint-disable-line no-unused-vars
+import { Context } from "probot/lib/context";
+import { WebhookPayloadPullRequest } from "@octokit/webhooks";
+
+const fetchConfig = (context: Context<WebhookPayloadPullRequest>) => {
+  const contentArgs = context.repo({ path: ".autorc", ref: context.payload.pull_request.head.ref });
+  return context.github.repos.getContents(contentArgs);
+};
 
 export = (app: Application) => {
+  app.on("pull_request.opened", async context => {
+    const config = await fetchConfig(context);
+    console.log(config);
+  });
+
   app.on("issues.opened", async context => {
     const issueComment = context.issue({ body: "Thanks for opening this issue!" });
     await context.github.issues.createComment(issueComment);
