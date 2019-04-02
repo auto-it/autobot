adde7dc3205e9aabc267756e9eb6209cdb0d4df7
Zephraph <zephraph@gmail.com>
2019-04-01 22:59:48 -0400

    Fix tests, add eslint/prettier

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,46 +1,49 @@
 // You can import your modules
 // import index from '../src/index'
 
-import nock from 'nock'
+import nock from "nock";
 // Requiring our app implementation
-import myProbotApp from '../src'
-import { Probot } from 'probot'
+import myProbotApp from "../src";
+import { Probot } from "probot";
 // Requiring our fixtures
-import payload from './fixtures/issues.opened.json'
-const issueCreatedBody = { body: 'Thanks for opening this issue!' }
+import payload from "./fixtures/issues.opened.json";
+const issueCreatedBody = { body: "Thanks for opening this issue!" };
 
-nock.disableNetConnect()
+nock.disableNetConnect();
 
-describe('My Probot app', () => {
-  let probot: any
+describe("My Probot app", () => {
+  let probot: Probot;
 
   beforeEach(() => {
-    probot = new Probot({ id: 123, cert: 'test' })
+    probot = new Probot({ id: 123, cert: "test" });
     // Load our app into probot
-    const app = probot.load(myProbotApp)
+    const app = probot.load(myProbotApp);
 
     // just return a test token
-    app.app = () => 'test'
-  })
+    app.app = {
+      getSignedJsonWebToken: () => "test",
+      getInstallationAccessToken: () => Promise.resolve("none"),
+    };
+  });
 
-  test('creates a comment when an issue is opened', async (done) => {
+  test("creates a comment when an issue is opened", async done => {
     // Test that we correctly return a test token
-    nock('https://api.github.com')
-      .post('/app/installations/2/access_tokens')
-      .reply(200, { token: 'test' })
+    nock("https://api.github.com")
+      .post("/app/installations/2/access_tokens")
+      .reply(200, { token: "test" });
 
     // Test that a comment is posted
-    nock('https://api.github.com')
-      .post('/repos/hiimbex/testing-things/issues/1/comments', (body: any) => {
-        done(expect(body).toMatchObject(issueCreatedBody))
-        return true
+    nock("https://api.github.com")
+      .post("/repos/hiimbex/testing-things/issues/1/comments", (body: any) => {
+        done(expect(body).toMatchObject(issueCreatedBody));
+        return true;
       })
-      .reply(200)
+      .reply(200);
 
     // Receive a webhook event
-    await probot.receive({ name: 'issues', payload })
-  })
-})
+    await probot.receive({ id: "123", name: "issues", payload });
+  });
+});
 
 // For more information about testing with Jest see:
 // https://facebook.github.io/jest/
