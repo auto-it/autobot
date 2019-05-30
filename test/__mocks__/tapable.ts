b5fd8abad8883d34a59e06f9c73bdb793bd15d62
Zephraph <zephraph@gmail.com>
2019-05-30 00:49:23 -0400

    Add new tests, do some funky mocking, clean up ts errors

---
+++ test/__mocks__/tapable.ts
@@ -0,0 +1,43 @@
+class BaseTestHook {
+  public canBail: boolean;
+  public isWaterfall: boolean;
+  public isSeries: boolean;
+  public isParallel: boolean;
+  public opts?: string[];
+
+  public constructor(name: string, opts?: string[]) {
+    this.canBail = name.includes("Bail");
+    this.isWaterfall = name.includes("Waterfall");
+    this.isSeries = name.includes("Series");
+    this.isParallel = !this.isSeries;
+    this.opts = opts;
+  }
+
+  public isUsed = jest.fn();
+}
+
+const testHookBuilder = (name: string) =>
+  name.startsWith("Sync")
+    ? class SyncTestHook extends BaseTestHook {
+        public constructor(opts?: string[]) {
+          super(name, opts);
+        }
+        public call = jest.fn().mockImplementation((...args) => {
+          expect(args.length).toBe((this.opts || []).length);
+        });
+      }
+    : class AsyncTestHook extends BaseTestHook {
+        public constructor(opts?: string[]) {
+          super(name, opts);
+        }
+        public promise = jest.fn().mockImplementation((...args) => {
+          expect(args.length).toBe((this.opts || []).length);
+        });
+      };
+
+export const SyncBailHook = testHookBuilder("SyncBailHook");
+export const SyncHook = testHookBuilder("SyncHook");
+export const AsyncSeriesWaterfallHook = testHookBuilder("AsyncSeriesWaterfallHook");
+export const AsyncSeriesHook = testHookBuilder("AsyncSeriesHook");
+export const AsyncParallelBailHook = testHookBuilder("AsyncParallelBailHook");
+export const AsyncParallelHook = testHookBuilder("AsyncParallelHook");
